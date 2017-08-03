ad4446f1a728cd3ab4265bbe4a1a87867f7e63e3
hackdecode- Jawand Singh <jawand.virk@gmail.com>
2017-08-04 00:51:50 +0530

    using RestSharp library as HttpClient was causing some issues. Added new methods to GetAuth Token, Change Location, GetMatches, Like a Matche

--- Netder/Program.cs
+++ Netder/Program.cs
@@ -1,12 +1,18 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Netder.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using static Netder.Models.AuthResponseVM;
+using static Netder.Models.MatchesVM;
 
 namespace Netder
 {
@@ -15,56 +21,163 @@ namespace Netder
         public static void Main(string[] args)
         {
             // get fbusrId and fbtoken
-
             string fbUserId = "";
             string fbToken = "";
+            
+            //Task.Run(async () =>
+            //{
+            //    await TinderAuth(fbUserId, fbToken);
+            //}).GetAwaiter().GetResult();
+            string authToken = TinderAuthentication(fbUserId, fbToken);
 
-            // make http request for auth token, use all headers and correct url
-            // get response 
-
-            Task.Run(async () =>
+            //TinderChangeLocation("28.655521", "77.117608", authToken);
+            
+            if (!string.IsNullOrEmpty(authToken))
             {
-                await TinderAuth(fbUserId, fbToken);
-            }).GetAwaiter().GetResult();
-
+                TinderGetMatches(authToken);
+            }
         }
 
-        public static async Task TinderAuth(string fbUserId, string fbToken)
+        public static void TinderChangeLocation(string lat, string lon, string authToken)
         {
-            try
+            var client = new RestClient("https://api.gotinder.com");
+
+            var request = new RestRequest("user/ping", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader("Content-Type", "application/json; charset=utf-8");
+            request.AddHeader("User-Agent", "Tinder Android Version 6.4.1");
+            request.AddHeader("X-Auth-Token", $"{authToken}");
+            request.AddHeader("os_version", "1935");
+            request.AddHeader("app-version", "371");
+            request.AddHeader("platform", "android");
+            request.AddHeader("Accept-Encoding", "gzip");
+
+            LocationChangeVM location = new LocationChangeVM()
             {
-                using (HttpClient httpClient = new HttpClient())
+                lat = lat,
+                lon = lon
+            };
+            request.AddBody(location);
+
+            var response = client.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string jsonResponse = response.Content.ToString();
+            }
+        }
+
+        public static void TinderGetMatches(string authToken)
+        {
+            // get a list of matches 
+
+            var client = new RestClient("https://api.gotinder.com");
+
+            var request = new RestRequest("user/recs", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader("Content-Type", "application/json; charset=utf-8");
+            request.AddHeader("User-Agent", "Tinder Android Version 6.4.1");
+            request.AddHeader("X-Auth-Token", $"{authToken}");
+            request.AddHeader("os_version", "1935");
+            request.AddHeader("app-version", "371");
+            request.AddHeader("platform", "android");
+            request.AddHeader("Accept-Encoding", "gzip");
+
+            var response = client.Execute(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string jsonResponse = response.Content.ToString();
+                MatchRootObject rootObject = JsonConvert.DeserializeObject<MatchRootObject>(jsonResponse);
+
+                var matches = rootObject.results.ToList();
+
+                List<string> matcheIds = new List<string>();
+
+                foreach (var item in matches)
                 {
-                    string requestPath = string.Format("/auth");
-                    httpClient.BaseAddress = new Uri("https://api.gotinder.com");
-                    httpClient.DefaultRequestHeaders.Accept.Clear();
-                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                    httpClient.DefaultRequestHeaders.Add("User-Agent", "Tinder Android Version 6.4.1");
-                    httpClient.DefaultRequestHeaders.Add("os_version", "1935");
-                    httpClient.DefaultRequestHeaders.Add("app-version", "371");
-                    httpClient.DefaultRequestHeaders.Add("platform", "android");
-                    httpClient.DefaultRequestHeaders.Add("Accept-Encoding", "gzip");
-                    //httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + "");
-
-                    JObject jObject = new JObject(new JProperty("facebook_id", fbUserId), new JProperty("facebook_token", fbToken));
-
-                    HttpContent content = new StringContent(jObject.ToString(), Encoding.UTF8, "application/json");
-
-                    HttpResponseMessage response = await httpClient.PostAsync(requestPath, content);
-                    
-                    if (response.IsSuccessStatusCode)
+                    if (item.gender == 1) // 0 = male, 1 = female
                     {
-                        string rawResponse = response.Content.ReadAsStringAsync().Result;
-                       //JObject resJsonObject = JObject.Parse(responseString);
+                        matcheIds.Add(item._id);
+                    }
+                    
+                }
+                int flag = 0;
+                foreach (var matcheId in matcheIds)
+                {
+                    // call api to like this id
+                    TinderLikeAMatche(matcheId, authToken);
+                    flag++;
+                    if (flag == matcheIds.Count)
+                    {
+                        TinderGetMatches(authToken); // not tested
                     }
                 }
             }
+            // loop through that list and call API which will swipe it right
+            // once list size is zero, then call again API to get matches.
+            // 
+        }
+
+        public static void TinderLikeAMatche(string matcheId, string authToken)
+        {
+            var client = new RestClient("https://api.gotinder.com");
+
+            var request = new RestRequest($"/like/{matcheId}", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader("Content-Type", "application/json; charset=utf-8");
+            request.AddHeader("User-Agent", "Tinder Android Version 6.4.1");
+            request.AddHeader("X-Auth-Token", $"{authToken}");
+            request.AddHeader("os_version", "1935");
+            request.AddHeader("app-version", "371");
+            request.AddHeader("platform", "android");
+            request.AddHeader("Accept-Encoding", "gzip");
+
+            var response = client.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string jsonResponse = response.Content.ToString();
+                Console.WriteLine($"response by Like A Match");
+            }
+        }
+
+
+        public static string TinderAuthentication(string fbUserId, string fbToken)
+        {
+            try
+            {
+                var client = new RestClient("https://api.gotinder.com");
+
+                var request = new RestRequest("auth", Method.POST);
+                request.RequestFormat = DataFormat.Json;
+                request.AddHeader("Content-Type", "application/json; charset=utf-8");
+                request.AddHeader("User-Agent", "Tinder Android Version 6.4.1");
+                request.AddHeader("os_version", "1935");
+                request.AddHeader("app-version", "371");
+                request.AddHeader("platform", "android");
+                request.AddHeader("Accept-Encoding", "gzip");
+
+                AuthRequestVM data = new AuthRequestVM()
+                {
+                    facebook_id = fbUserId,
+                    facebook_token = fbToken
+                };
+
+                request.AddBody(data);
+                string authToken = string.Empty;
+                var response = client.Execute(request);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    string jsonResponse = response.Content.ToString();
+                    RootObject rootObject = JsonConvert.DeserializeObject<RootObject>(jsonResponse);
+                    authToken = rootObject.token;
+                }
+                return authToken;
+            }
             catch (Exception ex)
             {
-
                 throw ex;
             }
-
         }
     }
 }
